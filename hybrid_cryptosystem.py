807b112f4512d6ff5474df4eeab98f189be4c62b
Alexey Zemel <loyxa.zemel@gmail.com>
2023-05-14 11:01:53 +0400

    added functions that save keys

--- hybrid_cryptosystem.py
+++ hybrid_cryptosystem.py
@@ -1,8 +1,8 @@
 import os
 import json
-import logging
 import argparse
 from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
 
 
 def load_settings(json_file: str) -> dict:
@@ -18,11 +18,8 @@ def load_settings(json_file: str) -> dict:
     try:
         with open(json_file) as json_file:
             settings = json.load(json_file)
-        logging.info(
-            f"Settings file successfully loaded from {json_file}")
     except OSError as err:
-        logging.warning(
-            f"Settings file wasn't loaded from {json_file}\n{err}")
+       raise err
     return settings
 
 
@@ -40,14 +37,54 @@ def generate_symmetric_key(length: int) -> bytes:
 
 
 def generate_asymmetric_keys() -> tuple:
-        """Generates an asymmetric key for asymmetric encryption algorithm.
+    """Generates an asymmetric key for asymmetric encryption algorithm.
 
-        Returns:
-            tuple: Asymmetric keys.
-        """
-        keys = rsa.generate_private_key(public_exponent=65537, key_size=2048)
-        private_key = keys
-        public_key = keys.public_key()
-        return private_key, public_key
+    Returns:
+        tuple: Asymmetric keys.
+    """
+    keys = rsa.generate_private_key(public_exponent=65537, key_size=2048)
+    private_key = keys
+    public_key = keys.public_key()
+    return private_key, public_key
 
 
+def save_symmetric_key(key: bytes, file_name: str) -> None:
+    """Saves a symmetric key to txt file.
+
+    Args:
+        key (bytes): Symmetric key.
+        file_name (str): Name of txt file.
+    """
+    try:
+        with open(file_name, 'wb') as key_file:
+            key_file.write(key)
+    except OSError as err:
+        raise err
+    
+    
+def save_private_key(private_key, file_name: str) -> None:
+    """Saves a private key to pem file.
+
+    Args:
+        private_key: Private key for asymmetric encoding algorithm.
+        file_name (str): Pem file for private key.
+     """
+    try:
+        with open(file_name, 'wb') as private_out:
+            private_out.write(private_key.private_bytes(encoding=serialization.Encoding.PEM, format=serialization.PrivateFormat.TraditionalOpenSSL, encryption_algorithm=serialization.NoEncryption()))
+    except OSError as err:
+        raise err
+        
+        
+def save_public_key(public_key, file_name:str)->None:
+    """Saves a public key to pem file.
+
+    Args:
+        public_key: Public key for asymmetric encoding algorithm.
+        file_name (str): Pem file for public key.
+    """
+    try:
+        with open(file_name, 'wb') as public_out:
+            public_out.write(public_key.public_bytes(encoding=serialization.Encoding.PEM, format=serialization.PublicFormat.SubjectPublicKeyInfo))
+    except OSError as err:
+        raise err
