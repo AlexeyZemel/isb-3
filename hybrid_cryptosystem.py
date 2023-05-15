7da9bbe74ae14154fd67ea56d4cdbdb0efefac9f
Alexey Zemel <loyxa.zemel@gmail.com>
2023-05-15 16:58:44 +0400

    made edits

--- hybrid_cryptosystem.py
+++ hybrid_cryptosystem.py
@@ -1,4 +1,5 @@
 import argparse
+import logging
 from load_write import load_settings
 from keys import create_keys
 from encrypt import encryption_text
@@ -8,28 +9,29 @@ from decrypt import decryption_text
 if __name__ == "__main__":
     settings = load_settings("settings.json")
     parser = argparse.ArgumentParser(description="Hybrid Cryptosystem")
-    parser.add_argument("-gen", "--generation", type=int,
-                        help="Запускает режим генерации ключей")
-    parser.add_argument("-enc", "--encryption",
-                        help="Запускает режим шифрования")
-    parser.add_argument("-dec", "--decryption",
-                        help="Запускает режим дешифрования")
+    parser.add_argument(
+        "-gen", "--generation", type=int, help="Запускает режим генерации ключей"
+    )
+    parser.add_argument("-enc", "--encryption", help="Запускает режим шифрования")
+    parser.add_argument("-dec", "--decryption", help="Запускает режим дешифрования")
     args = parser.parse_args()
     if args.generation:
         try:
             create_keys(args.generation, settings)
+            logging.info("Keys generation completed")
         except ValueError:
-            print("Invalid key length")
-        print("Keys generation completed")
+            logging.info(
+                "Invalid key length: the key length should be from 40 to 128 in 8-bit increments."
+            )
     elif args.encryption:
         try:
             encryption_text(settings)
+            logging.info("Encryption completed")
         except BaseException:
-            print("Something is wrong with the encryption key")
-        print("Encryption completed")
+            logging.info("Something is wrong with the encryption key")
     elif args.decryption:
         try:
             decryption_text(settings)
+            logging.info("Decryption completed")
         except BaseException:
-            print("Something is wrong with the decryption key")
-        print("Decryption completed")
+            logging.info("Something is wrong with the decryption key")
